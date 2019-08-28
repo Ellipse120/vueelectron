ab45f3939b485db910f08416f758d71afe410531
ellipse120 <ellipse120@gmail.com>
2019-08-28 18:53:49 +0800

    # init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-app>
+		<v-app-bar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span class="font-weight-light">Live Stream Demo</span>
+			</v-toolbar-title>
+		</v-app-bar>
+
+		<v-content>
+			<testTable/>
+			<HelloWorld/>
+		</v-content>
+	</v-app>
+</template>
+
+<script>
+	import HelloWorld from './components/HelloWorld';
+	import testTable from './components/table'
+
+	export default {
+		name: 'App',
+		components: {
+			HelloWorld,
+			testTable
+		},
+		data: () => ({
+			//
+		}),
+	};
+</script>
